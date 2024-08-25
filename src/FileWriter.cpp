dfd5cb6599097209c5262cdeb2a90018e1b2a3ec
Maxim <maxluibarskiy@gmail.com>
2024-08-25 11:37:17 +0300

    First step fileWriter

--- src/FileWriter.cpp
+++ src/FileWriter.cpp
@@ -1,24 +1,90 @@
 #include "FileWriter.h"
 #include <chrono>
+#include <thread>
 
+std::atomic<bool> FileWriter::isDone = false;
+std::atomic<int> FileWriter::threadCount = 0;
 //-----------------------------------------------------------
 FileWriter::FileWriter()
 {
     auto start = std::chrono::system_clock::now();
-
-    m_file =  std::ofstream("bulk" + std::to_string(start.time_since_epoch().count()) + ".txt");
+    m_fileName = "bulk" + std::to_string(start.time_since_epoch().count()) + ".txt";
+    m_file = std::ofstream(m_fileName);
 }
 //-----------------------------------------------------------
 FileWriter::~FileWriter()
 {
     m_file.close();
 }
+
+void FileWriter::write_block(const std::string &command, const std::string &fileName)
+{
+    std::ofstream file(fileName, std::ofstream::out | std::ofstream::app);
+
+    if(command.empty())
+        return;
+
+    file << command << " ";
+}
+
+void FileWriter::write(CommandQueue& queue, const std::string &fileName)
+{
+    threadCount++;
+
+    while (!queue.isEmpty())
+    {
+        auto block = queue.popCommand();
+        write_block(block, fileName);
+    }
+
+    threadCount--;
+    if(threadCount == 0)
+        FinishUpdate(fileName);
+}
 //-----------------------------------------------------------
 void FileWriter::update(const Data &data)
 {
-    for (const auto& chunk : data)
-    {
-        m_file << ' ' << chunk;
-    }
-    m_file << std::endl;
+    isDone = false;
+    threadCount = 0;
+
+    if (data.empty())
+        return;
+
+    CommandQueue queue(data);
+
+    std::thread file_1(write, std::ref(queue), m_fileName);
+    std::thread file_2(write, std::ref(queue), m_fileName);
+
+    file_1.join();
+    file_2.join();
+}
+void FileWriter::FinishUpdate( const std::string &fileName)
+{
+    std::ofstream file(fileName, std::ofstream::out | std::ofstream::app);
+    file << std::endl;
+}
+
+//-----------------------------------------------------------
+CommandQueue::CommandQueue(const Data &data)
+{
+    for (const auto& chunk : data   )
+        m_queue.push(chunk);
+}
+
+std::string CommandQueue::popCommand()
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+
+    if(m_queue.empty())
+        return "";
+
+    auto result = m_queue.front();
+    m_queue.pop();
+
+    return result;
+}
+
+bool CommandQueue::isEmpty()
+{
+    return m_queue.empty();
 }
--- src/FileWriter.h
+++ src/FileWriter.h
@@ -1,17 +1,40 @@
 #ifndef BULK_FILEWRITER_H
 #define BULK_FILEWRITER_H
+
 #include "IObserver.h"
 #include <fstream>
+#include <queue>
+#include <mutex>
+#include <atomic>
+
+class CommandQueue
+{
+public:
+    CommandQueue(const Data &data);
+    std::string popCommand();
+    bool isEmpty();
+private:
+    std::queue<std::string> m_queue;
+    std::mutex m_mutex;
+};
 
 class FileWriter : public IObserver
 {
 public:
     FileWriter();
-    ~FileWriter();
+    ~FileWriter() override;
     void update(const Data &data) override;
-
 private:
-    std::ofstream m_file;
+
+    static void write(CommandQueue& queue, const std::string &fileName);
+    static void write_block(const std::string& command, const std::string &fileName);
+    static void FinishUpdate( const std::string &fileName);
+
+    std::string     m_fileName;
+    std::ofstream   m_file;
+
+    static std::atomic<bool>    isDone;
+    static std::atomic<int>     threadCount;
 };
 
 #endif //BULK_FILEWRITER_H
